2bf89020b2a77489ff238022ad2432af594389db
Sangeet Manghnani <sangeet.manghnani@gmail.com>
2012-12-17 23:11:12 +0530

    Update src/index.cpp

--- src/index.cpp
+++ src/index.cpp
@@ -9,8 +9,17 @@ Index::~Index()
 }
 
 bool Index::AddEntry(string filename, string word)
-{
-	/* TODO: Check if wordFileMap already contains the word. If it does
+{  
+	if(wordFileMap.bool empty()const = true) 
+          {
+          	//creates a new string vectory
+          }         
+    else
+       {
+       	
+       }
+
+/* TODO: Check if wordFileMap already contains the word. If it does
  	 * then add the file name to the string vectory against the word. If it
  	 * does not contain the word, then create a new string vectory and add
  	 * it to the map with the word as the key. */
